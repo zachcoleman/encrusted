7ca82bf8e44085e25ec22c88d8a584bded08d7cf
Zachary Coleman <zacharywcoleman@gmail.com>
2022-10-29 16:50:54 -0600

    CICD trying again

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use numpy::*;
-use pyo3::types::PyBytes;
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
 
-// old JSON serde (slow) 
+// old JSON serde (slow)
 // #[pyfunction]
 // fn encode_mask(_py: Python, arr: PyReadonlyArray2<u8>) -> String{
 //     json!({
@@ -11,15 +11,15 @@ use pyo3::prelude::*;
 // }
 
 #[pyfunction]
-fn encode_mask(py: Python, arr: PyReadonlyArray2<u8>) -> PyObject{
+fn encode_mask(py: Python, arr: PyReadonlyArray2<u8>) -> PyObject {
     let bytes = bincode::serialize(&arr.to_owned_array()).unwrap();
     PyBytes::new(py, &bytes).into()
 }
 
 #[pyfunction]
-fn decode_mask<'a>(py: Python<'a>, bytes: &'a PyAny) -> PyResult<&'a PyArray2<u8>>{
+fn decode_mask<'a>(py: Python<'a>, bytes: &'a PyAny) -> PyResult<&'a PyArray2<u8>> {
     let bytes: Vec<u8> = bytes.extract::<Vec<u8>>().unwrap();
-    let arr: ndarray::Array2::<u8> = bincode::deserialize(&bytes).unwrap();
+    let arr: ndarray::Array2<u8> = bincode::deserialize(&bytes).unwrap();
     Ok(PyArray2::from_array(py, &arr))
 }
 
