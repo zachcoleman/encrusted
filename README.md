2908445cd988f33a595b774f68aaa39526f96b5c
Zachary Coleman <zacharywcoleman@gmail.com>
2022-10-29 15:18:30 -0600

    initial 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# encrusted
-Testing Rust-based mask encoding in Python
+![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/zachcoleman/encrusted/tests/main)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/encrusted)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/encrusted)
+[![License](https://img.shields.io/badge/license-Apache2.0-green)](./LICENSE)
+
+# encrusted 
+The project was developed using the [maturin](https://maturin.rs) framework.
+
+## Installation
+From PyPI:
+```shell
+pip install encrusted
+```
+
+Build from source:
+```
+maturin build -r -i=path/to/python
+pip install .../fast-stats/target/wheels/<whl file name>.whl
+```
+
+## Running Tests
+Tests are run with `pytest`.
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use numpy::*;
+use pyo3::types::PyBytes;
+use pyo3::prelude::*;
+
+// old JSON serde (slow) 
+// #[pyfunction]
+// fn encode_mask(_py: Python, arr: PyReadonlyArray2<u8>) -> String{
+//     json!({
+//         "mask": arr.to_owned_array()
+//     }).to_string()
+// }
+
+#[pyfunction]
+fn encode_mask(py: Python, arr: PyReadonlyArray2<u8>) -> PyObject{
+    let bytes = bincode::serialize(&arr.to_owned_array()).unwrap();
+    PyBytes::new(py, &bytes).into()
+}
+
+#[pyfunction]
+fn decode_mask<'a>(py: Python<'a>, bytes: &'a PyAny) -> PyResult<&'a PyArray2<u8>>{
+    let bytes: Vec<u8> = bytes.extract::<Vec<u8>>().unwrap();
+    let arr: ndarray::Array2::<u8> = bincode::deserialize(&bytes).unwrap();
+    Ok(PyArray2::from_array(py, &arr))
+}
+
+#[pymodule]
+fn encrusted(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(encode_mask, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_mask, m)?)?;
+    Ok(())
+}
